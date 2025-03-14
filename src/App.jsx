d8229c986a4889cbcabbd2c2451d147fb5fc9547
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-14 12:47:50 -0300

    03-Stage display
    03-Stage display

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { MyContext } from './context/index.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/app.css'
 
@@ -5,10 +7,19 @@ import Stage1 from './components/stage_one';
 import Stage2 from './components/stage_two';
 
 const App = () => {
+  const context = useContext(MyContext);
+
   return(
-    <>
-      App
-    </>
+    <div className="wrapper">
+      <div className="center-wrapper">
+          <h1>Who pays the bill</h1>
+          { context.stage === 1?
+            <Stage1/>
+          :
+            <Stage2/>
+          }
+      </div>
+    </div>
   )
 }
 
